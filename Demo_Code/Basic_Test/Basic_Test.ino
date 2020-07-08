2510b8ccc171cacb5f9f078656808689144071bf
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-08 19:51:05 +0300

    Update Basic_Test.ino
    I had uploaded the wrong source code :astonished:
    I have fixed that :laughing:

--- Demo_Code/Basic_Test/Basic_Test.ino
+++ Demo_Code/Basic_Test/Basic_Test.ino
@@ -1,38 +1,72 @@
-/********************************************************
- * PID Basic Example
- * Reading analog input 0 to control analog PWM output 3
- ********************************************************/
+#include <Servo.h>
 
-#include <PID_v1.h>
+Servo my_servo;
 
-#define PIN_INPUT 2
-#define PIN_OUTPUT 3
+// defines pins numbers
+const int trigPin = 2;
+const int echoPin = 3;
+// defines variables
+long duration;
+int distance;
 
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-
-//Specify the links and initial tuning parameters
-double Kp=2;
-double Ki=5;
-double Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-
-void setup()
-{
-  //initialize the variables we're linked to
-  Input = digitalRead(PIN_INPUT);
-  Setpoint = 100;
-
-  Serial.begin(9600);
-
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
+void setup() {
+  my_servo.attach(9);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
 }
-
-void loop()
-{
-  Input = analogRead(PIN_INPUT);
-  myPID.Compute();
-  analogWrite(PIN_OUTPUT, Output);
-  Serial.println(Output);
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  for(int i=30; i<= 130; i++){
+    // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+    my_servo.write(i);
+    delay(25);
+    }
+  
+  for(int i=130; i>= 30; i--){
+    // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+    my_servo.write(i);
+    delay(25);
+    }
+    
 }
